b0828fa7f685f5f3b6e3adda42e11875d0e640fc
allm <allmacdev@gmail.com>
2026-03-27 18:24:24 -0300

    feat: add Swagger API documentation setup

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
+import { AppModule } from './app.module';
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -9,6 +12,15 @@ import { ZodValidationPipe } from 'nestjs-zod';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('SpotHub API')
+    .setDescription("")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
