1fa8122bd0e455b4e823bd509b7496a763f6cc77
allm <allmacdev@gmail.com>
2026-03-29 15:26:34 -0300

    refactor(user): update controller to use PaginationDto

--- app/backend/src/user/user.controller.ts
+++ app/backend/src/user/user.controller.ts
@@ -10,7 +10,7 @@ import {
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
-import { QueryUserDto } from './dto/queryUser.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { UserDto } from './dto/user-response.dto';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/updateUser.dto';
@@ -37,7 +37,7 @@ export class UserController {
   @ApiUnauthorizedResponse({
     description: 'Unauthorized - invalid or missing JWT token',
   })
-  async getAllUser(@Query() query: QueryUserDto) {
+  async getAllUser(@Query() query: PaginationDto) {
     return this.userService.getAll(query);
   }
 
