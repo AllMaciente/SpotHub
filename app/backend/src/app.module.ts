5527b03fd00abd39f0893f0d0f348516e763e8ec
allm <allmacdev@gmail.com>
2026-03-29 16:56:16 -0300

    feat(app): add ReservationsModule to app imports

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { HashService } from './hash/hash.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { RoomsModule } from './rooms/rooms.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, RoomsModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, RoomsModule, ReservationsModule],
   controllers: [],
   providers: [SnowflakeService, PrismaService, HashService],
 })
